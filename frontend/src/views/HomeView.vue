739ee648c4cc449745a962005cca16c7ed96e959
Lloyd Encallado <lloydencallado@gmail.com>
2025-08-21 18:56:54 +0800

    initialized frontend framework, tailwind and shadcn-vue

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue';
+</script>
+
+<template>
+  <main class="text-center p-4">
+    <h1 class="text-3xl font-bold underline">
+      Welcome to FastVue Fullstack Template
+    </h1>
+    <p class="mt-4">This is a simple Vue.js application using Vite, Vue Router, and Tailwind CSS.</p>
+    <p class="mt-2">
+      You can start building your application by editing the files in the <code>src</code> directory.
+    </p>
+  </main>
+</template>
